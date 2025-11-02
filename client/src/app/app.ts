9b22b375e4c4dbcff7f914baf55e78d424c2f201
nhungoc-2907 <ngocltn23411@st.uel.edu.vn>
2025-11-03 01:37:01 +0700

    header

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { TestData } from "./pages/test-data/test-data";
 import { JsonPipe } from '@angular/common';
+import { RouterOutlet } from '@angular/router'
+import { Header } from './pages/header/header';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TestData, JsonPipe],
+  imports: [TestData, JsonPipe,RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
