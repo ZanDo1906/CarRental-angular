820b7713a52b05254848454bf912a1f58301c069
ZanDo1906 <94846421+ZanDo1906@users.noreply.github.com>
2025-11-03 00:46:59 +0700

    update data

--- client/src/app/interfaces/Admin.ts
+++ client/src/app/interfaces/Admin.ts
@@ -6,8 +6,5 @@ export interface iAdmin {
     Mat_khau: string;
     Vai_tro: string; //   "3" admin
     Anh_dai_dien: string;
-    Ngay_tao: string;
-    Can_cuoc_cong_dan?: string;    // chỉ có cho thuê & chủ xe
-    Giay_phep_lai_xe?: string;     // chỉ có cho thuê & chủ xe
-    So_lan_vi_pham?: number;       // chỉ có cho thuê & chủ xe
+    Ngay_tao: string
 }
