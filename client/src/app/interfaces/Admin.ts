f824fa1f4fb32c388066407143847089095e152b
ZanDo1906 <94846421+ZanDo1906@users.noreply.github.com>
2025-11-03 00:58:38 +0700

    update data

--- client/src/app/interfaces/Admin.ts
+++ client/src/app/interfaces/Admin.ts
@@ -4,7 +4,6 @@ export interface iAdmin {
     So_dien_thoai: string;
     Email: string;
     Mat_khau: string;
-    Vai_tro: string; //   "3" admin
     Anh_dai_dien: string;
     Ngay_tao: string
 }
