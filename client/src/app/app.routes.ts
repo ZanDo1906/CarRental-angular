6d6a92dcdcab5f584b556fedaf86651af92dbd36
Nhi Nguyen <nhinguyen@MacBook-Pro-cua-Nguyen-5.local>
2025-11-04 16:41:32 +0700

    Thêm route /ve-trust-car và sửa navbar để mở VeTrustCar

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { CarList } from './pages/carList/carList';
 import { CarDetail } from './pages/car-detail/car-detail';
 import { TroThanhChuXe } from './pages/tro-thanh-chu-xe/tro-thanh-chu-xe';
 import { DKChoThueXe } from './pages/dk-cho-thue-xe/dk-cho-thue-xe';
+import { VeTrustCar } from './pages/ve-trust-car/ve-trust-car';
 
 export const routes: Routes = [
     { path: '', component: CarList },          // trang danh sách
     { path: 'xe/:id', component: CarDetail },  // trang chi tiết
     { path: 'tro-thanh-chu-xe', component: TroThanhChuXe }, // trang trở thành chủ xe
     { path: 'dk-cho-thue-xe', component: DKChoThueXe }, // trang đăng ký cho thuê xe
+    { path: 've-trust-car', component: VeTrustCar }, // trang vé tin cậy
     { path: '**', redirectTo: '' },
 
 ];
