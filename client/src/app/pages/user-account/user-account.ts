c104ee39fd015adbdc307b395bbace74368c86d9
Nhi Nguyen <nhinguyen@MacBook-Pro-cua-Nguyen-5.local>
2025-11-05 18:21:28 +0700

    Update UserAccount

--- client/src/app/pages/user-account/user-account.ts
+++ client/src/app/pages/user-account/user-account.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SideBar } from '../side-bar/side-bar';
+
 
 @Component({
   selector: 'app-user-account',
-  imports: [],
+  imports: [CommonModule, RouterModule, SideBar],
   templateUrl: './user-account.html',
   styleUrl: './user-account.css',
 })
